748f80d8613f3250ca52aa05cc1e68fef763e939
GRD <d.grebelny@gmail.com>
2023-10-14 22:58:48 +0400

    .env && @nestjs/config && docker-compose && mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -6,9 +8,25 @@ import { TopPageModule } from './top-page/top-page.module';
 import { ProductModule } from './product/product.module';
 import { ReviewModule } from './review/review.module';
 
+const config = new ConfigService();
+const pathToMongo = config.get('PATH_TO_MONGO');
+console.log('\napp.module pathToMongo = ', pathToMongo);
+
 @Module({
-  imports: [AuthModule, TopPageModule, ProductModule, ReviewModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('PATH_TO_MONGO')
+      }),
+      inject: [ConfigService]
+    }),
+    AuthModule,
+    TopPageModule,
+    ProductModule,
+    ReviewModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
