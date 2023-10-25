5f01965356dec1b1eb090593783484e9f3da9702
GRD <d.grebelny@gmail.com>
2023-10-25 06:14:37 +0400

    docker-compose.yml container names

--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -15,24 +15,12 @@ import { ReviewService } from './review.service';
 import { REVIEW_NOT_FOUND } from './review.constants';
 import { ReviewModel } from './review.model';
 
-/* eslint-disable import/order */
-import { ConfigService } from '@nestjs/config';
-
 @Controller('review')
 export class ReviewController {
-  constructor(private readonly reviewService: ReviewService, private configService: ConfigService) {}
+  constructor(private readonly reviewService: ReviewService) {}
 
   @Post('create')
   async create(@Body() dto: CreateReviewDto, @Req() request: Request): Promise<ReviewModel> {
-    console.log('\nReviewController dto', dto);
-    console.log('request.body:', request.body);
-    console.log('MONGO_LOGIN', this.configService.get('MONGO_LOGIN'));
-    console.log('MONGO_PASSWORD', this.configService.get('MONGO_PASSWORD'));
-    console.log('MONGO_HOST', this.configService.get('MONGO_HOST'));
-    console.log('MONGO_PORT', this.configService.get('MONGO_PORT'));
-    console.log('MONGO_DATABASE', this.configService.get('MONGO_DATABASE'));
-    console.log('RAVOLY', this.configService.get('RAVOLY'));
-
     return this.reviewService.create(dto);
   }
 
