f996ea189547098727704c0a2c5a7b0c3a5cc4be
GRD <d.grebelny@gmail.com>
2023-10-27 21:12:39 +0400

    src/review/review.service.spec.ts unit test

--- src/review/review.service.spec.ts
+++ src/review/review.service.spec.ts
@@ -1,12 +1,19 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getModelToken } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
 import { ReviewService } from './review.service';
 
 describe('ReviewService', () => {
   let service: ReviewService;
+  const exec = { exec: jest.fn() };
+  const MockedReviewModel = (): any => ({ find: () => exec });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ReviewService],
+      providers: [
+        ReviewService,
+        { useFactory: MockedReviewModel, provide: getModelToken('ReviewModel') }
+      ]
     }).compile();
 
     service = module.get<ReviewService>(ReviewService);
@@ -14,5 +21,12 @@ describe('ReviewService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('findByProductId - success', async () => {
+    const id = new Types.ObjectId().toHexString();
+    MockedReviewModel().find().exec.mockReturnValue([{ productId: id }]);
+    const res = await service.findByProductId(id);
+    expect(res[0].productId).toBe(id);
   });
 });
--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
+import { Model } from 'mongoose';
 import { ReviewDocument, ReviewModel } from './review.model';
 import { CreateReviewDto } from './dto/create-review.dto';
 
