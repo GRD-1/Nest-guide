7f70aa882b1ce21ddf9e38db7931214552013980
GRD <d.grebelny@gmail.com>
2023-11-14 03:04:17 +0400

    src/pipes/id-validation-pipe.ts update

--- src/pipes/id-validation-pipe.ts
+++ src/pipes/id-validation-pipe.ts
@@ -1,14 +1,13 @@
 import { PipeTransform, Injectable, ArgumentMetadata, BadRequestException } from '@nestjs/common';
+import { Types } from 'mongoose';
+import { ID_VALIDATION_ERROR } from './id-validation.constants';
 
 @Injectable()
 export class IdValidationPipe implements PipeTransform<string> {
   async transform(value: string, metadata: ArgumentMetadata): Promise<string> {
-    if (metadata.type !== 'param' || metadata.metatype !== String) {
-      throw new BadRequestException('Invalid parameter type');
-    }
-    const validFormats = [/^[0-9a-fA-F]{12}$/, /^[0-9a-fA-F]{24}$/, /^\d+$/];
-    if (!validFormats.some((regex) => regex.test(value))) {
-      throw new BadRequestException('Invalid ID format');
+    if (metadata.type !== 'param') return value;
+    if (!Types.ObjectId.isValid(value)) {
+      throw new BadRequestException(ID_VALIDATION_ERROR);
     }
     return value;
   }
