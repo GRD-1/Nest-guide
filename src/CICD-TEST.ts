8919169baf9401621d4e27ed673184c39def8e53
GRD <d.grebelny@gmail.com>
2023-11-25 21:40:38 +0400

    ci: plug plugin lint-stage
    plug plug plug plug plug
    plug plug

--- src/CICD-TEST.ts
+++ src/CICD-TEST.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-class CICDTEST   {
+class CICDTEST {
   testMethod(a: any): void {
-    console.log('text text text text')
-    const uselessConst = 25
-
+    console.log('text text text text');
+    const uselessConst = 25;
   }
-
-
-
-
 }
