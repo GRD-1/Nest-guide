026b6b83f4a2d307c4fb68085f47122a829bbf35
GRD <d.grebelny@gmail.com>
2023-11-30 03:26:39 +0400

    ci: test the dammit commit.workflow

--- src/CICD-TEST-2.ts
+++ src/CICD-TEST-2.ts
@@ -1,6 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 
-export class CICDTEST     {
+export class CICDTEST       {
   testMethod(a: string): void {
     console.log('text text text text');
     const uselessConst = 25;
