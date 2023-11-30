6afd5b005fbea02ee4b9aad645c0fe2062b7c4a0
GRD <d.grebelny@gmail.com>
2023-11-30 04:43:27 +0400

    ci(.github/workflows): check the commit by eslint
    .github/workflows/commit.workflow.yml
    check the commit by eslint and prettier on the github side

--- src/CICD-TEST-2.ts
+++ src/CICD-TEST-2.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 
-export class CICDTEST {
+export class CICDTEST     {
   testMethod(a: string): void {
     console.log('text text text text');
     const uselessConst = 25;
+
+
+
   }
 }
