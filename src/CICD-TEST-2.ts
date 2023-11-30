71fb315815afc8c62c2cfa773dd71b5e8d005236
GRD <d.grebelny@gmail.com>
2023-11-30 06:33:55 +0400

    ci(.github/workflows): check the commit by eslint
    test eslint functionalities

--- src/CICD-TEST-2.ts
+++ src/CICD-TEST-2.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 
-export class CICDTEST     {
+export class CICDTEST {
   testMethod(a: string): void {
-    console.log('text text text text')
-    const uselessConst = 25
-
-
-
-
-
-
+    console.log('text text text text');
+    const uselessConst = 25;
   }
 }
