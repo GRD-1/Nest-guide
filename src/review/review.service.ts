6e40c475e130ce0532c2846a19106b547705b3f4
GRD <d.grebelny@gmail.com>
2023-12-03 18:33:45 +0400

    ci(.github/workflows): fix review.service
    fix review.service.ts type coercion

--- src/review/review.service.ts
+++ src/review/review.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { async } from 'rxjs';
 import { ReviewDocument, ReviewModel } from './review.model';
 import { CreateReviewDto } from './dto/create-review.dto';
 
@@ -13,11 +14,11 @@ export class ReviewService {
     return newReview.save();
   }
 
-  async delete(id: string): Promise<ReviewDocument | null> {
+  async delete(id: string): Promise<ReviewModel | null> {
     return this.reviewModel.findByIdAndDelete(id).exec();
   }
 
-  async findByProductId(productId: string): Promise<ReviewDocument[]> {
+  async findByProductId(productId: string): Promise<ReviewModel[]> {
     return this.reviewModel.find({ productId }).exec();
   }
 
