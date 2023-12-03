9df6000ec926c9598962f5eb8ac8591b9965a8b6
GRD <d.grebelny@gmail.com>
2023-12-03 19:14:27 +0400

    break(.github/workflows): create a new release
    create a new release

--- config/git/commitlint.config.js
+++ config/git/commitlint.config.js
@@ -32,7 +32,7 @@ module.exports = {
     'type-enum': [
       2,
       'always',
-      ['build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'chore']
+      ['break', 'build', 'ci', 'docs', 'feat', 'fix', 'perf', 'refactor', 'revert', 'style', 'test', 'chore']
     ]
   }
 };
